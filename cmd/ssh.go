95de98a73cc125f5de647965ea528bad9be6cc99
v3nividiv1ci <v3nividiv1ci@outlook.com>
2021-07-19 20:28:30 +0800

    login (very very simple version)

--- cmd/ssh.go
+++ cmd/ssh.go
@@ -17,22 +17,39 @@ package cmd
 
 import (
 	"fmt"
-
+	"github.com/kirinlabs/HttpRequest"
 	"github.com/spf13/cobra"
 )
 
 // sshCmd represents the ssh command
 var sshCmd = &cobra.Command{
 	Use:   "ssh",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "to log in a pic-bed's server ",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("ssh called")
+		url := "http://localhost:8080/api/auth/login"
+		name, password := args[0], args[1]
+		fmt.Println(name, password)
+		resp, err := HttpRequest.Post(url,
+			map[string]interface{}{
+				"username": name,
+				"password": password,
+			})
+		if err != nil {
+			panic(err)
+		}
+		defer func(resp *HttpRequest.Response) {
+			err := resp.Close()
+			if err != nil {
+
+			}
+		}(resp)
+
+		body, err := resp.Body()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(body))
+
 	},
 }
 
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module go-picbed-cli
 go 1.16
 
 require (
+	github.com/gin-gonic/gin v1.7.2 // indirect
+	github.com/kirinlabs/HttpRequest v1.1.1 // indirect
 	github.com/spf13/cobra v1.2.1 // indirect
 	github.com/spf13/viper v1.8.1 // indirect
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
